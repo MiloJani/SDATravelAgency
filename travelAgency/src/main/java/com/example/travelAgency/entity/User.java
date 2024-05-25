aade50ba3c596a1e624e2b06be30bf51b34a523c
MiloJani <emiljano.molla2@gmail.com>
2024-05-25 15:25:00 +0200

    Maybe some little changes to User

--- travelAgency/src/main/java/com/example/travelAgency/entity/User.java
+++ travelAgency/src/main/java/com/example/travelAgency/entity/User.java
@@ -3,6 +3,8 @@ package com.example.travelAgency.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
@@ -22,11 +24,15 @@ public class User {
     @Column(nullable = false,length = 30)
     private String password;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "role_id", referencedColumnName = "roleId")
-    private Role role;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> role;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "client_id", referencedColumnName = "clientId")
     private Client client;
 }
