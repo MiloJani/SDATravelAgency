b6f1dd960d68a4698b3f473b7d8f1c83bf277aef
MiloJani <emiljano.molla2@gmail.com>
2024-05-25 11:40:15 +0200

    First commit

--- travelAgency/src/main/java/com/example/travelAgency/TravelAgencyApplication.java
+++ travelAgency/src/main/java/com/example/travelAgency/TravelAgencyApplication.java
@@ -1,13 +1,13 @@
-package com.example.travel;
+package com.example.travelAgency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelApplication {
+public class TravelAgencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
+		SpringApplication.run(TravelAgencyApplication.class, args);
 	}
 
 }
--- travelAgency/src/test/java/com/example/travelAgency/TravelAgencyApplicationTests.java
+++ travelAgency/src/test/java/com/example/travelAgency/TravelAgencyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.travel;
+package com.example.travelAgency;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TravelApplicationTests {
+class TravelAgencyApplicationTests {
 
 	@Test
 	void contextLoads() {
