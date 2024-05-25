adb1bc04e46a95ae177b0e3fdba43535c08a4ab7
Kliti <Kliticabej23@gmail.com>
2024-05-25 15:01:59 +0200

    Updated tour and category

--- travelAgency/src/main/java/com/example/travelAgency/entity/Category.java
+++ travelAgency/src/main/java/com/example/travelAgency/entity/Category.java
@@ -1,40 +1,25 @@
 package com.example.travelAgency.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Category")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="Id", nullable = false)
+    @Column(name = "Id", nullable = false)
 
     private Long categoryId;
-    @Column(name="Category Name ", nullable=false)
+    @Column(name = "Category Name ", nullable = false)
     private String categoryName;
-    public Category() {
 
-    }
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
--- travelAgency/src/main/java/com/example/travelAgency/entity/Tour.java
+++ travelAgency/src/main/java/com/example/travelAgency/entity/Tour.java
@@ -1,13 +1,19 @@
 package com.example.travelAgency.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
+
 @Entity
+@Getter
+@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Tour")
-
-
 public class Tour {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,84 +34,5 @@ public class Tour {
     private String endingCity;
 
 
-
-    public Tour() {
-
-    }
-
-    public Tour(Long id, String tourName, Category categoryId, LocalDate startDate, LocalDate endDate, Double price, String startingCity, String endingCity) {
-        this.id = id;
-        this.tourName = tourName;
-        this.categoryId = categoryId;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.price = price;
-        this.startingCity = startingCity;
-        this.endingCity = endingCity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTourName() {
-        return tourName;
-    }
-
-    public void setTourName(String tourName) {
-        this.tourName = tourName;
-    }
-
-    public Category getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Category categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getStartingCity() {
-        return startingCity;
-    }
-
-    public void setStartingCity(String startingCity) {
-        this.startingCity = startingCity;
-    }
-
-    public String getEndingCity() {
-        return endingCity;
-    }
-
-    public void setEndingCity(String endingCity) {
-        this.endingCity = endingCity;
-    }
 }
 
