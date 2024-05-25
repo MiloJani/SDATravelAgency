3c50fad3da4e2d573080b888cbddde3066453790
megi3 <megikana5@gmail.com>
2024-05-25 14:58:05 +0200

    ADDED REVIEWS

--- travelAgency/src/main/java/com/example/travelAgency/entity/Reviews.java
+++ travelAgency/src/main/java/com/example/travelAgency/entity/Reviews.java
@@ -1,4 +1,29 @@
 package com.example.travelAgency.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reviews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reviewId;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_id")
+    private Tour tour;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+
 }
+
