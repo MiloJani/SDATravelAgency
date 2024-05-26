e1e8d126bcbbcc3dd873d3fa88418b10a97fa06c
KleaRuci <klearuci11@gmail.com>
2024-05-26 14:04:43 +0200

    Added tour CRUD

--- travelAgency/src/main/java/com/example/travelAgency/service/TourService.java
+++ travelAgency/src/main/java/com/example/travelAgency/service/TourService.java
@@ -1,4 +1,19 @@
 package com.example.travelAgency.service;
 
+import com.example.travelAgency.dto.CategoryDTO;
+import com.example.travelAgency.entity.Category;
+import com.example.travelAgency.entity.Tour;
+
+import java.util.List;
+
 public interface TourService {
+    List<Tour> getAllTours();
+
+    Tour getTourById(Long id) ;
+
+    Tour addTour(Tour tour) ;
+
+    Tour updateTour(Tour tour, Long id) ;
+
+    Long deleteTour(Long id) ;
 }
--- travelAgency/src/main/java/com/example/travelAgency/service/impl/TourServiceImpl.java
+++ travelAgency/src/main/java/com/example/travelAgency/service/impl/TourServiceImpl.java
@@ -1,4 +1,61 @@
 package com.example.travelAgency.service.impl;
 
-public class TourServiceImpl {
+import com.example.travelAgency.entity.Tour;
+import com.example.travelAgency.repository.TourRepository;
+import com.example.travelAgency.service.TourService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TourServiceImpl implements TourService {
+
+    private final TourRepository tourRepository;
+
+    public TourServiceImpl(TourRepository tourRepository) {
+        this.tourRepository = tourRepository;
+    }
+
+
+    @Override
+    public List<Tour> getAllTours() {
+        return tourRepository.findAll();
+    }
+
+    @Override
+    public Tour getTourById(Long id) {
+        return tourRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Tour addTour(Tour tour) {
+        if (!tourRepository.existsById(tour.getTourId())){
+            tourRepository.save(tour);
+            return tour;
+        }
+       return null;
+    }
+
+    @Override
+    public Tour updateTour(Tour tour, Long id) {
+        Optional<Tour> tourOptional=tourRepository.findById(id);
+        if(tourOptional.isPresent()){
+            Tour existingTour=tourOptional.get();
+            existingTour.setTourName(tour.getTourName());
+            tourRepository.save(existingTour);
+            return existingTour;
+
+        }
+        return null;
+    }
+
+    @Override
+    public Long deleteTour(Long id) {
+        Optional<Tour> tourOptional=tourRepository.findById(id);
+        if(tourOptional.isPresent()) {
+            Tour existingTour=tourOptional.get();
+            tourRepository.delete(existingTour);
+            return id;
+        }
+        return null;
+    }
 }
