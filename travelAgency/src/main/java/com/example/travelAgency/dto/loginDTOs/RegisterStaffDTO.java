818634d904b3b1301f80cca5050b69fd5295c812
MiloJani <emiljano.molla2@gmail.com>
2024-06-08 13:58:43 +0200

    Added empty payment entity

--- travelAgency/src/main/java/com/example/travelAgency/dto/loginDTOs/RegisterStaffDTO.java
+++ travelAgency/src/main/java/com/example/travelAgency/dto/loginDTOs/RegisterStaffDTO.java
@@ -16,7 +16,7 @@ public class RegisterStaffDTO extends LoginDTO {
     private String staffName;
 
     @NotBlank(message = "Phone number is mandatory")
-    @Pattern(regexp = "\\+?[0-9. ()-]{7,25}", message = "Phone number is invalid")
+//    @Pattern(regexp = "\\+?[0-9. ()-]{7,25}", message = "Phone number is invalid")
     @Schema(example = "+393331234567")
     private String phoneNumber;
 
