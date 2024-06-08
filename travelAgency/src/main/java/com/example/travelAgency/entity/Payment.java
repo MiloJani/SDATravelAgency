43235c03941d34cdc342307d2b35df9a86519467
GjolindBiba <gjolindbiba123@gmail.com>
2024-06-08 14:02:51 +0200

    added payment

--- travelAgency/src/main/java/com/example/travelAgency/entity/Payment.java
+++ travelAgency/src/main/java/com/example/travelAgency/entity/Payment.java
@@ -1,4 +1,56 @@
-package com.example.travelAgency.entity;
+package com.example.booking.dataproviders.entities;
 
+import com.example.travelAgency.entity.Order;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "PAYMENT")
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long paymentId;
+
+//    @Column(name = "PAYMENT_TYPE")
+//    @NotBlank(message = "Payment type is required")
+//    private String paymentType;
+
+    @Column(name = "CARD_NUMBER")
+    @NotBlank(message = "Card number is required")
+    private String cardNumber;
+
+    @Column(name = "CARD_HOLDER_NAME")
+    @NotBlank(message = "Card holder name is required")
+    private String cardHolderName;
+
+    @Column(name = "EXPIRATION_YEAR")
+    @Min(value = 2024, message = "Expiration year must be greater than or equal to 2022")
+    private int expirationYear;
+
+    @Column(name = "EXPIRATION_MONTH")
+    @Min(value = 1, message = "Expiration month must be between 1 and 12")
+    @Max(value = 12, message = "Expiration month must be between 1 and 12")
+    private int expirationMonth;
+
+    @Column(name = "CVV")
+    @PositiveOrZero(message = "CVV must be a positive number or zero")
+    private int cvv;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orderId")
+    private Order order;
+
 }
