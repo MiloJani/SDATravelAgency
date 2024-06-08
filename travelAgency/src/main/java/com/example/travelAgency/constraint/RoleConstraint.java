aa63041e673fa27affcba5ce2b4537f66546ec46
AjlaTaka <ajlataka199@gmail.com>
2024-06-08 10:39:47 -0700

    edit roll constraints

---
+++ travelAgency/src/main/java/com/example/travelAgency/constraint/RoleConstraint.java
@@ -0,0 +1,8 @@
+package com.example.travelAgency.constraint;
+
+public class RoleConstraint {
+    private static final String admin = "Admin not found";
+    private static final String staff = "Staff not found";
+    private static final String manager = "Manager not found";
+    private static final String user = "User not found";
+}
